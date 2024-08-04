85023e5f0f9c6f519e0d2686408d03c9956e10fe
daroo <darooritaruntej@gmail.com>
2024-08-04 13:13:12 -0600

    Updated config file.

--- src/main/java/com/application/seller/config/SecurityConfig.java
+++ src/main/java/com/application/seller/config/SecurityConfig.java
@@ -1,6 +1,10 @@
 package com.application.seller.config;
+import com.application.seller.service.SellerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,9 +22,19 @@ public class SecurityConfig{
         return new BCryptPasswordEncoder();
     }
 
+//   @Autowired
+//   private SellerService sellerService;
+
+//    @Bean
+//    public AuthenticationProvider authProvider(){
+//        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+//        provider.setUserDetailsService(sellerService);
+//        return provider;
+//    }
+
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(customizer -> customizer.disable())
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers(
                                 new AntPathRequestMatcher("/api/seller/register"),
